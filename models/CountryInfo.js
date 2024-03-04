dc2c63469438241530d4c372decf7830def22945
Guillaume SUCH <guillaume.such@gmail.com>
2024-03-04 09:12:19 +0100

    models

---
+++ models/CountryInfo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Sous-document pour les consulats
+const consulateSchema = new Schema({
+    address: { type: String },
+    phone: { type: String },
+    email: { type: String },
+    emergency_phone: { type: String }
+});
+
+// Schéma du modèle pour les informations sur l'ambassade
+const embassySchema = new Schema({
+    address: { type: String },
+    phone: { type: String },
+    email: { type: String },
+    emergency_phone: { type: String }
+});
+
+// Modèle pour les informations du pays
+const countryInfoSchema = new Schema({
+    country: { type: String, unique, required: true },
+    embassy: embassySchema,
+    consulate: [consulateSchema],
+    emergency_number: { type: String },
+    police_number: { type: String },
+    firefighter_number: { type: String },
+    member_112: { type: Boolean }
+});
+
+// Export du modèle CountryInfo
+const CountryInfo = mongoose.model('CountryInfo', countryInfoSchema);
+module.exports = CountryInfo;

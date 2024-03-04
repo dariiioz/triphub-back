f7cc7f11ef3d1f711d27c35d3e7930cc3fa285b7
CAPPELLE Aymeric <cappelleaymeric@proton.me>
2024-03-04 09:33:19 +0100

    add cors & require connect-database

--- models/connect-database.js
+++ models/connect-database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = process.env.MONGO_URI;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))

b6e26e9075d20da09e9c02b71d53262255e18ac7
CAPPELLE Aymeric <cappelleaymeric@proton.me>
2024-03-04 17:34:59 +0100

    add countriesinfos routes

--- models/country-info.js
+++ models/country-info.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Sous-document pour les consulats
@@ -6,7 +6,7 @@ const consulateSchema = new Schema({
     address: { type: String },
     phone: { type: String },
     email: { type: String },
-    emergency_phone: { type: String }
+    emergency_phone: { type: String },
 });
 
 // Schéma du modèle pour les informations sur l'ambassade
@@ -14,20 +14,20 @@ const embassySchema = new Schema({
     address: { type: String },
     phone: { type: String },
     email: { type: String },
-    emergency_phone: { type: String }
+    emergency_phone: { type: String },
 });
 
 // Modèle pour les informations du pays
 const countryInfoSchema = new Schema({
-    country: { type: String, unique, required: true },
+    country: { type: String, unique: true, required: true },
     embassy: embassySchema,
     consulate: [consulateSchema],
     emergency_number: { type: String },
     police_number: { type: String },
     firefighter_number: { type: String },
-    member_112: { type: Boolean }
+    member_112: { type: Boolean },
 });
 
 // Export du modèle CountryInfo
-const CountryInfo = mongoose.model('CountryInfo', countryInfoSchema);
+const CountryInfo = mongoose.model("CountriesInfos", countryInfoSchema);
 module.exports = CountryInfo;
--- routes/countries-infos.js
+++ routes/countries-infos.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const CountryInfo = require("../models/country-info");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.post("/addCountry", async (req, res) => {
+    const newCountry = new CountryInfo({
+        country: "France",
+    });
+
+    const countrySaved = await newCountry.save();
+
+    res.status(200).json({ result: true, data: countrySaved });
 });
 
 module.exports = router;
